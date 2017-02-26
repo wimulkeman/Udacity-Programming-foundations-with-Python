a7bc9301d3385fd64dc07f785e9a0705eb886c6a
Wim Ulkeman <wimulkeman@hotmail.com>
2017-02-26 01:01:38 +0100

    Finishing the movie website database assingment.

--- code/movies/entertainment_center.py
+++ code/movies/entertainment_center.py
@@ -1,10 +1,34 @@
 import media
+import fresh_tomatoes
 
-toy_store = media.Novie(
+toy_store = media.Movie(
     'Toy Story',
     'A story of a boy and his toys that come to life',
-    'http://upload.wikipedia.org/wikipedia/en/1/13/Toy_Story.jpg',
-    'https://www.youtube.com/watch?v=vwy2H85NQC4'
+    'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+    'https://www.youtube.com/watch?v=KYz2wyBy3kc'
 )
 
-print(toy_store.storyline)
+avatar = media.Movie(
+    'Avatar',
+    'A marine on an alien planet',
+    'https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg',
+    'https://www.youtube.com/watch?v=5PSNL1qE6VY'
+)
+
+sherlock_holmes = media.Movie(
+    'Sherlock Holmes',
+    'Nothing escapes them',
+    'https://upload.wikimedia.org/wikipedia/en/e/e0/Sherlock_holmes_ver5.jpg',
+    'https://www.youtube.com/watch?v=J7nJksXDBWc'
+)
+
+movies = [toy_store, avatar, sherlock_holmes]
+# fresh_tomatoes.open_movies_page(movies)
+# print media.Movie.VALID_RATINGS
+# print media.Movie.__doc__
+
+# Output: Movie
+print media.Movie.__name__
+
+# Output: media
+print media.Movie.__module__
